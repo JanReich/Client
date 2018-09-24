29f5904caa5756eb529b5d7ef963afdbb3ab4a35
Mycachu24 <43515039+Mycachu24@users.noreply.github.com>
2018-09-24 21:49:18 +0200

    Genius Michael

--- src/main/ReadyButton.java
+++ src/main/ReadyButton.java
@@ -2,12 +2,14 @@ package main;
 
 import graphics.interfaces.BasicInteractableObject;
 import toolBox.DrawHelper;
+import toolBox.Inputmanager;
 
 import javax.imageio.ImageIO;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.awt.event.MouseEvent;
 
 
 public class ReadyButton implements BasicInteractableObject {
@@ -19,17 +21,19 @@ public class ReadyButton implements BasicInteractableObject {
             private int width;
             private int height;
             private boolean active;
+            private Inputmanager ready;
 
                 //Referenzen
             private String username;
             private BufferedImage image;
 
-        public ReadyButton(String username, int x, int y, int width, int height, boolean active) {
+        public ReadyButton(String username, int x, int y, int width, int height, boolean active,Inputmanager ready) {
 
             this.x = x;
             this.y = y;
             this.width = width;
             this.height = height;
+            this.ready = ready;
 
             this.active = active;
             this.username = username;
@@ -57,6 +61,13 @@ public class ReadyButton implements BasicInteractableObject {
 
                 //Damit nur der eigene Button gedrückt werden kann.
             if(active) {
+
+                if(ready.isInside(e,x,y,width,height)){
+
+                    System.out.println("Ich bin bereit");
+
+                }
+
 
 
             }
