e8ddb75f51cc6a35b2fb04fa09cf5eb6bba7fae5
jan.reich@mail.de <SIMS32000reich>
2018-09-24 23:05:37 +0200

    Final update für den Tag - 24.09.2018

--- src/main/GameManager.java
+++ src/main/GameManager.java
@@ -5,90 +5,86 @@ import graphics.interfaces.ManagementObject;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
-public class GameManager implements ManagementObject {
+    public class GameManager implements ManagementObject {
 
-                //Attribute
-            private int clientID;
+                    //Attribute
+                private int clientID;
+                private String myUsername;
+                private boolean gameStarted;
 
-                //Referenzen
-            private Display display;
-            private GameClient client;
-            private ArrayList<Integer> onlineClientIDs;
+                    //Referenzen
+                private Display display;
+                private GameClient client;
 
-            private String myUsername;
-            private boolean gameStarted;
+                    //Buttons
+                private ReadyButton button1;
+                private ReadyButton button2;
+                private ReadyButton button3;
 
-                //Buttons
-            private ReadyButton button1;
-            private ReadyButton button2;
-            private ReadyButton button3;
+        public GameManager(Display display, GameClient client, String username, int clientID) {
 
-    public GameManager(Display display, GameClient client, String username, int clientID) {
+                this.client = client;
+                this.display = display;
 
-            this.client = client;
-            this.display = display;
+                this.clientID = clientID;
+                this.myUsername = username;
 
-            this.clientID = clientID;
-            this.myUsername = username;
-
-            onlineClientIDs = new ArrayList<>();
-            addReadybutton(username, clientID, true);
-        }
-
-        @Override
-        public void update(double delta) {
-
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        public void addReadybutton(String username, int clientID, boolean active) {
-
-            switch (clientID) {
-
-                case 1:
-                    button1 = new ReadyButton(username,10,400, 200, 75 , active);
-                    display.getActivePanel().drawObjectOnPanel(button1);
-                    break;
-                case 2:
-                    button2 = new ReadyButton(username,710,400, 200, 75 , active);
-                    display.getActivePanel().drawObjectOnPanel(button2);
-                    break;
-                case 3:
-                    button3 = new ReadyButton(username, 350,10, 200, 75 , active);
-                    display.getActivePanel().drawObjectOnPanel(button3);
-                    break;
+                addReadybutton(username, clientID, true);
             }
-        }
 
-        public void removeReadybutton(int clientID) {
+            @Override
+            public void update(double delta) {
 
-            switch (clientID) {
-
-                case 1:
-                    display.getActivePanel().removeObjectFromPanel(button1);
-                    break;
-                case 2:
-                    display.getActivePanel().removeObjectFromPanel(button2);
-                    break;
-                case 3:
-                    display.getActivePanel().removeObjectFromPanel(button3);
-                    break;
             }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            public void addReadybutton(String username, int clientID, boolean active) {
+
+                switch (clientID) {
+
+                    case 1:
+                        button1 = new ReadyButton(username,10,400, 200, 75 , active);
+                        display.getActivePanel().drawObjectOnPanel(button1);
+                        break;
+                    case 2:
+                        button2 = new ReadyButton(username,710,400, 200, 75 , active);
+                        display.getActivePanel().drawObjectOnPanel(button2);
+                        break;
+                    case 3:
+                        button3 = new ReadyButton(username, 350,10, 200, 75 , active);
+                        display.getActivePanel().drawObjectOnPanel(button3);
+                        break;
+                }
+            }
+
+            public void removeReadybutton(int clientID) {
+
+                switch (clientID) {
+
+                    case 1:
+                        display.getActivePanel().removeObjectFromPanel(button1);
+                        break;
+                    case 2:
+                        display.getActivePanel().removeObjectFromPanel(button2);
+                        break;
+                    case 3:
+                        display.getActivePanel().removeObjectFromPanel(button3);
+                        break;
+                }
+            }
+
+        public boolean isGameStarted() {
+
+            return gameStarted;
         }
-
-    public boolean isGameStarted() {
-
-        return gameStarted;
     }
-}
